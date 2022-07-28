9163bdc8fcde4fd4b2de451bf6a3d6f95967afbe
Zain Ridzuan <zain.ridzuan@gmail.com>
2022-07-29 05:14:14 +1000

    Added basic code for 1 client to connect to server

--- client.py
+++ client.py
@@ -9,3 +9,58 @@ Client needs to accept the following three arguments:
 Each client needs to be initiated as:
 -  python3 client.py server_IP server_port client_udp_server_port
 '''
+
+from socket import *
+import json
+import sys
+
+def login(username, password, client_socket):
+    request = {
+        'request_type': 'login',
+        'username': username,
+        'password': password
+    }
+
+    while 1:
+        client_socket.send(bytes(json.dumps(request).encode('utf-8')))
+
+        response = client_socket.recv(1024)
+        response = response.decode('utf-8')
+        # We wait to receive the reply from the server, store it in response
+        
+        if response == 'Success':
+            print("Login Successful!")
+            break
+        elif response == 'Failure':
+            print('Invalid username and/or password. Please try again.')
+            request['username'] = input('Enter username: ')
+            request['password'] = input('Enter password: ')
+        elif response == 'Blocked':
+            print('Invalid password. Your account has been blocked due to multiple unsuccessful login attempts. Please try again later.')
+            exit()
+        else:
+            print(response)
+
+
+def connect_to_server(server_name, server_port):
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((server_name, server_port))
+
+    login_success = 0
+
+
+    username = input('Enter username: ')
+    password = input('Enter password: ')
+    login(username, password, client_socket)
+
+    client_socket.close()
+    # and close the socket
+
+if __name__ == '__main__':
+    #if len(sys.argv) != 3:
+    #    print('Try python3 client.py <server_IP> <server_port>')
+    #    exit()
+    
+    server_IP = sys.argv[1]
+    server_port = int(sys.argv[2])
+    connect_to_server(server_IP, server_port)
--- server/server.py
+++ server/server.py
@@ -8,5 +8,64 @@ Server needs to accept the following arguments:
    should be blocked for 10 second.
 The server needs to be ran before the clients. Needs to be initiated as:
 -  python3 server.py server_port number_of_consecutive_failed_attempts
+
+Easy way to do message types is to use json 
+{
+   'request_type': 'example_request',
+   'info': '...',
+   'info1': '...'
+}
 '''
 
+from socket import * 
+from threading import Thread
+import json
+import sys
+
+def start_server(server_port, number_of_consecutive_failed_attempts):
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind(('localhost', server_port))
+    server_socket.listen(1)
+
+    print("The server is ready to receive...")
+
+    while 1:
+        connection_socket, addr = server_socket.accept()
+        payload = connection_socket.recv(1024)
+        request = json.loads(payload.decode('utf-8'))
+        request_type = request['request_type']
+        username = request['username']
+        password = request['password']
+
+        # creating credentials dictionary with username as key and password as value
+        credentials = {}
+        with open('credentials.txt') as credential_list:
+            for line in credential_list:
+                userpass = line.split()
+                u = userpass[0]
+                p = userpass[1].strip()
+                credentials[u] = p
+
+        login_attempts = dict.fromkeys(credentials, 0)
+
+        if request_type == 'login':
+            if username in credentials:
+                if login_attempts[username] >= number_of_consecutive_failed_attempts:
+                    connection_socket.send(bytes('Blocked', encoding='utf-8'))
+                elif password == credentials[username]:
+                    connection_socket.send(bytes('Success', encoding='utf-8'))
+                elif password != credentials[username]:
+                    connection_socket.send(bytes('Failure', encoding='utf-8'))
+                    login_attempts[username] = login_attempts[username] + 1
+                    print(login_attempts)
+            else:
+                connection_socket.send(bytes('Invalid username', encoding='utf-8'))
+
+    connection_socket.close()
+    # Close the connection_socket. Note that the serverSocket is still alive waiting for new clients to connect, we are 
+    # only closing the connection_socket.
+
+if __name__ == "__main__":
+    server_port = int(sys.argv[1])
+    number_of_consecutive_failed_attempts = int(sys.argv[2])
+    start_server(server_port, number_of_consecutive_failed_attempts)
